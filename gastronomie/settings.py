2d9e8b15920a04e686f7bb5b6546513d21f75c42
anaikasmile <eliora20147@gmail.com>
2017-09-28 23:37:50 +0000

    Modif database default

--- gastronomie/settings.py
+++ gastronomie/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#l3#%g^k@g9wjtsnzkwce-7uy92_%1x=)gt%t_o_+fd!7%g($3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
