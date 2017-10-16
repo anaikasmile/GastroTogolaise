d4cf3e78b932396fe5692c188c1a2e25af801325
anaikasmile <eliora20147@gmail.com>
2017-10-16 22:17:12 +0000

    add google authentication settings

--- gastronomie/settings.py
+++ gastronomie/settings.py
@@ -104,7 +104,7 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
-SOCIALACCOUNT_PROVIDERS = \
+SOCIALACCOUNT_PROVIDERS = [
     {'facebook':
        {'METHOD': 'oauth2',
         'SCOPE': ['email','public_profile', 'user_friends'],
@@ -133,7 +133,7 @@ SOCIALACCOUNT_PROVIDERS = \
         }
       }
 
-    
+    ]
 
 ROOT_URLCONF = 'gastronomie.urls'
 
