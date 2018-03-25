9c71cf422d411c27a034cdfd958410b212b94e8c
anaikasmile <eliora20147@gmail.com>
2018-03-25 18:23:46 +0000

    Change setting

--- gastronomie/development.py
+++ gastronomie/development.py
@@ -1,6 +1,27 @@
-from .base import *
- 
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# Database
-#https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+SECRET_KEY = '#l3#%g^k@g9wjtsnzkwce-7uy92_%1x=)gt%t_o_+fd!7%g($3'
 
+#DEBUG = config('DEBUG', default=False, cast=bool)
+
+DEBUG = True
+
+#DATABASES = {'default': dj_database_url.config(default="postgres://qfshiulqbogrll:11e2a4a020aaca0707ecf132d0a0685caf76a7cb07feb6383a44771377fe8804@ec2-54-221-212-208.compute-1.amazonaws.com:5432/dfqktli8ama0hd")}
+
+ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'cookit_db', 
+        'USER': 'cookit_user', 
+        'PASSWORD': 'cookit_user', 
+        'HOST': 'localhost', 
+        'PORT': '', 
+    }
+}
+
+# Application definition
+SITE_ID = 2
--- gastronomie/settings.py
+++ gastronomie/settings.py
@@ -20,33 +20,44 @@ from decouple import config, Csv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
 SECRET_KEY = '#l3#%g^k@g9wjtsnzkwce-7uy92_%1x=)gt%t_o_+fd!7%g($3'
 
-#DEBUG = config('DEBUG', default=False, cast=bool)
 
-DEBUG = True
-
-#DATABASES = {'default': dj_database_url.config(default="postgres://qfshiulqbogrll:11e2a4a020aaca0707ecf132d0a0685caf76a7cb07feb6383a44771377fe8804@ec2-54-221-212-208.compute-1.amazonaws.com:5432/dfqktli8ama0hd")}
+DATABASES = {'default': dj_database_url.config(default="postgresql-angular-86634")}
 
 ALLOWED_HOSTS = ['*']
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': 'cookit_db', 
-        'USER': 'cookit_user', 
-        'PASSWORD': 'cookit_user', 
-        'HOST': 'localhost', 
-        'PORT': '', 
-    }
+# Application definition
+SITE_ID = 3
+
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'   
+
+
+#Files storages
+AWS_STATIC_STORAGE_BUCKET_NAME = 'tgcook'
+AWS_STORAGE_BUCKET_NAME = 'tgcook'
+
+AWS_ACCESS_KEY_ID = 'AKIAIOWSHLAWROUESPLQ'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
 }
 
-# Application definition
-SITE_ID = 2
+#STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+#STATIC_URL = S3_URL
+MEDIA_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+
+
+
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
