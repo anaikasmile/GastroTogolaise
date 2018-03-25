9cb4ce9db93ec69dfae2832dfa003a261f2a81a7
anaikasmile <eliora20147@gmail.com>
2018-03-25 18:10:46 +0000

    Reset du projet

---
+++ gastronomie/development.py
@@ -0,0 +1,6 @@
+from .base import *
+ 
+
+# Database
+#https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+
--- gastronomie/settings.py
+++ gastronomie/settings.py
@@ -23,14 +23,27 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '#l3#%g^k@g9wjtsnzkwce-7uy92_%1x=)gt%t_o_+fd!7%g($3'
 
-DEBUG = config('DEBUG', default=False, cast=bool)
+#DEBUG = config('DEBUG', default=False, cast=bool)
 
-
+DEBUG = True
 
 #DATABASES = {'default': dj_database_url.config(default="postgres://qfshiulqbogrll:11e2a4a020aaca0707ecf132d0a0685caf76a7cb07feb6383a44771377fe8804@ec2-54-221-212-208.compute-1.amazonaws.com:5432/dfqktli8ama0hd")}
 
+ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
+        'NAME': 'cookit_db', 
+        'USER': 'cookit_user', 
+        'PASSWORD': 'cookit_user', 
+        'HOST': 'localhost', 
+        'PORT': '', 
+    }
+}
 
 # Application definition
 SITE_ID = 2
@@ -124,7 +137,7 @@ ROOT_URLCONF = 'gastronomie.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':[os.path.join(BASE_DIR, '../templates')],
+        'DIRS':[os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -209,15 +222,15 @@ LOGIN_REDIRECT_URL = "/"
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'../staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'../media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '../gastronomie/static'),
-    os.path.join(BASE_DIR, '../media'),
+    os.path.join(BASE_DIR, 'gastronomie/static'),
+    os.path.join(BASE_DIR, 'media'),
     
 )
 
