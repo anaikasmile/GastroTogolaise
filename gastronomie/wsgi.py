ab9b48759912d2758c71a5a37ff3fbf17f2165b6
root <anaikasmile@github.com>
2018-11-22 19:45:25 +0000

    Modif settings

--- gastronomie/wsgi.py
+++ gastronomie/wsgi.py
@@ -14,7 +14,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gastronomie.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gastronomie.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
