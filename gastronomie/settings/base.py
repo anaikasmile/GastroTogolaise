ff03b19eea1d4aaff26bfc8f99c6421900ec9946
anaikasmile <eliora20147@gmail.com>
2018-11-24 23:27:47 +0000

    User logo efault modifie

--- gastronomie/settings/base.py
+++ gastronomie/settings/base.py
@@ -216,8 +216,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'static'),
-     os.path.join(BASE_DIR, 'media'),
-     '/home/togokuizine/lib/python2.7/site-packages/django/contrib/admin/static',
+     #os.path.join(BASE_DIR, 'media'),
+     #'/home/togokuizine/lib/python2.7/site-packages/django/contrib/admin/static',
     #'/home/sonia/Venv/lib/python3.5/site-packages/django/contrib/admin/static/admin',
 )
 
